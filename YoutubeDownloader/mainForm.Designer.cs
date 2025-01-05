b031f3b89f8c4d9b6ea7f723071f36f07b348035
Ferhat COŞKUN <ghostfrt@gmail.com>
2025-01-05 23:54:41 +0300

    git ignore file added

---
+++ YoutubeDownloader/mainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace YoutubeDownloader
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 12);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "youtube video url si girin";
+            textBox1.Size = new Size(470, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(399, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 3000;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(23, 55);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Kayıt Dizini Değiştir";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // mainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(512, 105);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "mainForm";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Youtube Video Downloader";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private Button button2;
+    }
+}
---
+++ YoutubeDownloader/mainForm.cs
@@ -0,0 +1,157 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+using System.Diagnostics;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        string folderPath = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1_ClickAsync(sender, e);
+            // StatusLabel'a bilgi mesajı ekleyin
+      
+
+
+        }
+
+
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+
+            // YouTube URL'sini al
+            string videoUrl = textBox1.Text.Trim();
+
+            // Eğer URL boşsa uyarı ver
+            if (string.IsNullOrEmpty(videoUrl))
+            {
+                MessageBox.Show("Lütfen bir YouTube URL'si girin.");
+                return;
+            }
+
+
+            // Klasör seçmek için FolderBrowserDialog kullanıyoruz
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+                {
+                    folderDialog.Description = "Bir klasör seçin";
+                    if (folderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        folderPath = folderDialog.SelectedPath;
+                   
+                    }
+                }
+            }
+            await DownloadAndConvertToMp3(videoUrl, folderPath);
+        }
+
+        private async Task DownloadAndConvertToMp3(string videoUrl, string folderPath)
+        {
+            button1.Enabled = false;
+            try
+            {
+                this.Text = "Video İndiriliyor..";
+                // YouTubeExplode kütüphanesi ile video bilgilerini al
+                var youtube = new YoutubeClient();
+                Application.DoEvents();
+                var video = await youtube.Videos.GetAsync(videoUrl);
+                Application.DoEvents();
+                // Video başlığını dosya adı olarak kullan
+                string videoTitle = video.Title;
+                string validFileName = GetValidFileName(videoTitle); // Geçerli bir dosya adı oluştur
+
+                // Klasörde video başlığına göre dosya yolu
+                string tempFilePath = Path.Combine(folderPath, validFileName + ".mp4");
+
+                // Video akışlarını al
+                var streamInfo = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
+                var audioStreamInfo = streamInfo.GetAudioStreams().GetWithHighestBitrate();
+                Application.DoEvents();
+                // Akışı indir
+                var audioStream = await youtube.Videos.Streams.GetAsync(audioStreamInfo);
+
+                // Video dosyasını geçici olarak kaydet
+                using (var fileStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    await audioStream.CopyToAsync(fileStream);
+                }
+                this.Text = "Video İndirildi.. Mp3 Dönüştürülüyor..";
+
+                // MP3 dosyasına dönüştürme işlemi
+                string mp3FilePath = Path.Combine(folderPath, validFileName + ".mp3");
+                ConvertToMp3(tempFilePath, mp3FilePath);
+
+                // Geçici MP4 dosyasını sil
+                File.Delete(tempFilePath);
+                this.Text = "Video başarıyla indirildi ve MP3 formatında kaydedildi.";
+                //MessageBox.Show("Video başarıyla indirildi ve MP3 formatında kaydedildi.");
+                textBox1.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}");
+            }
+            finally
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void ConvertToMp3(string filePath, string mp3FilePath)
+        {
+            try
+            {
+
+                // MP4 dosyasını MP3'e dönüştürme (NAudio ile)
+                using (var reader = new MediaFoundationReader(filePath))
+                {
+                    Application.DoEvents();
+                    using (var writer = new LameMP3FileWriter(mp3FilePath, reader.WaveFormat, 128))
+                    {
+                        reader.CopyTo(writer);
+                        Application.DoEvents();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Dönüştürme hatası: {ex.Message}");
+            }
+        }
+
+        // Dosya adında geçerli olmayan karakterleri kaldırmak için yardımcı fonksiyon
+        private string GetValidFileName(string fileName)
+        {
+            // Geçerli dosya adı oluşturmak için geçersiz karakterleri temizleyelim
+            foreach (char c in Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_');
+            }
+            return fileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Klasör seçmek için FolderBrowserDialog kullanıyoruz
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "Bir klasör seçin";
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    folderPath = folderDialog.SelectedPath;
+                }
+            }
+        }
+    }
+}
